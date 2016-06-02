fcb23dbb5bdd85aa4b4cc9144219bf6e84f65470
sarahbones <sarahlee517@gmail.com>
2016-06-02 15:00:38 +0800

    create MSManagedObject subclasses

---
+++ Mr Ride/Mr Ride/Models/Locations.swift
@@ -0,0 +1,17 @@
+//
+//  Locations.swift
+//  Mr Ride
+//
+//  Created by Sarah on 6/2/16.
+//  Copyright © 2016 AppWorks School Sarah Lee. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Locations: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
---
+++ Mr Ride/Mr Ride/Models/RideHistory+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  RideHistory+CoreDataProperties.swift
+//  Mr Ride
+//
+//  Created by Sarah on 6/2/16.
+//  Copyright © 2016 AppWorks School Sarah Lee. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension RideHistory {
+
+    @NSManaged var date: String?
+    @NSManaged var distance: NSNumber?
+    @NSManaged var tatalTime: NSNumber?
+    @NSManaged var weight: NSNumber?
+    @NSManaged var locations: NSSet?
+
+}
